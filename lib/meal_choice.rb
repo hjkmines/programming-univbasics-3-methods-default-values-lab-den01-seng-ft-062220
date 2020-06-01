2ae4c638f82954386f3d5f4ee405936bdd9564ee
Hyung Kim <hjkmines@gmail.com>
2020-06-01 05:06:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,8 +5,7 @@
 
 def meal_choice (veg1, veg2, protein = "tofu")
 
-puts "What a nutritious meal!"
-
+convert puts "What a nutritious meal!"
 return "A plate of #{protein} with #{veg1} and #{veg2}."
 
 end 
